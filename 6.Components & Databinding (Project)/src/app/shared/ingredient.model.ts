9596b6b2ef7683301e2c2e66e0da939ab6696628
anshbegmirza <anshbegmirza@gmail.com>
2025-02-04 11:24:18 +0530

    Continuing Angular learning

---
+++ 6.Components & Databinding (Project)/src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  // Rather than doing the above thing you can simply do this
+  constructor(public name: string, public amount: number) {}
+}
